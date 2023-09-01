e64f0ae669ae3082101d2799d6143d01b205dbb7
0x1Luffy <chetankesare06@gmail.com>
2023-09-02 01:22:06 +0530

    styles the div, arranged some of the divs.

--- app/page.js
+++ app/page.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>testing</div>
+    <>
+    <div className='wrapper h-full w-full flex items-center justify-center'>
+      <div className='title w-full h-36 bg-black text-center font-extrabold text-8xl p-4 text-white'>MY TO-DO-LIST</div>
+    </div>
+
+      <div className='main-wrapper flex justify-center items-center'>
+        <form className='bg-gray-700 w-full h-[760px] m-10 rounded-xl'>
+
+        </form>
+      </div>
+      </>
   )
 }
 
