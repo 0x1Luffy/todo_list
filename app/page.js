ebde06076db148af966994d4b9edb9503aa6e89f
0x1Luffy <chetankesare06@gmail.com>
2023-09-03 01:49:20 +0530

    made a react variable to render the task onto the screen.

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("")
   const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
   const submitHandler=(e)=>{
     e.preventDefault();
             console.log(title)
@@ -12,7 +13,10 @@ const page = () => {
             settitle("")
             setdesc("")
   }
+
+  let renderTask = <h3>NO TASK AVAILABLE</h3>
   return (
+    
     <>
     <div className='wrapper h-full w-full flex items-center justify-center'>
       <div className='title w-full h-36 bg-black text-center font-extrabold text-8xl p-4 text-white'>MY TO-DO-LIST</div>
@@ -37,7 +41,9 @@ const page = () => {
           <input type='submit' value={"ADD TASK"}  className=' h-16 w-28 border-green-400 bg-zinc-600 text-white font-extrabold border-2 rounded-md ml-10 mt-28' />
         </form>
         <div className='h-20 w-[1833px] bg-cyan-500 absolute flex items-center justify-center font-extrabold text-black text-3xl'>
-                <h3>NO TASK AVAILABLE</h3>
+                <ul>
+                  {renderTask}
+                </ul>
       </div>
       </div>
       </>
