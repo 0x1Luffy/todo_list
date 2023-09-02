eaf55f29c53d5cdd7f8c9484f124134d6a0b1c9b
0x1Luffy <chetankesare06@gmail.com>
2023-09-02 14:40:28 +0530

    Update the form layout and styles .

--- app/page.js
+++ app/page.js
@@ -8,10 +8,10 @@ const page = () => {
     </div>
 
       <div className='main-wrapper flex justify-center items-center'>
-        <form className='bg-gray-700 w-full h-[760px] m-10 rounded-xl flex justify-center items-center flex-col'>
-          <input type='text' placeholder='Enter Your Task ' className='text-2xl border-green-400 border-2 rounded-md'></input>
-          <input type='text' placeholder='Explain Your Task ' className='text-2xl border-green-400 border-2 rounded-md'></input>
-          <input type='button'  className='h-5 w-5 border-green-400 border-2 rounded-md'></input>
+        <form className='bg-gray-700 w-full h-[760px] m-10 rounded-xl flex justify-center'>
+          <input type='text' placeholder='Enter Your Task ' className=' h-32 w-80 text-2xl border-green-400 border-2 rounded-md mt-20 text-center'></input>
+          <input type='text' placeholder='Explain Your Task ' className='  h-32 w-80 text-2xl border-green-400 border-2 rounded-md ml-10 mt-20 text-center'></input>
+          <input type='submit'  className=' h-16 w-28 border-green-400 bg-zinc-600 text-white font-extrabold border-2 rounded-md ml-10 mt-28'></input>
         </form>
       </div>
       </>
